355f4dc68db9c887ed7243853db6900831652e8b
suntao <727924283@qq.com>
2020-05-22 10:04:48 +0800

    初始化

---
+++ WB_swiftTests/WB_swiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  WB_swiftTests.swift
+//  WB_swiftTests
+//
+//  Created by ishop02 on 2020/5/22.
+//  Copyright © 2020 happy. All rights reserved.
+//
+
+import XCTest
+@testable import WB_swift
+
+class WB_swiftTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
